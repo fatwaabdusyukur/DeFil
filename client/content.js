404912b9a4d405f208c9d39c3a6d8685bca3aee4
fatwaabdusyukur <waengyoo@gmail.com>
2023-04-04 14:30:35 +0700

    add new feature and fix predict function

--- client/content.js
+++ client/content.js
@@ -1,3 +1,51 @@
+function waitForElm(selector) {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver((mutations) => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
+
+waitForElm(".css-1dbjc4n.r-1awozwy.r-18u37iz.r-1s2bzr4").then((el) => {
+  const filterBtn = new Image();
+  filterBtn.src = "https://cdn-icons-png.flaticon.com/512/9584/9584673.png";
+  filterBtn.width = 17;
+  filterBtn.height = 17;
+  filterBtn.style.cursor = "pointer";
+  el.appendChild(filterBtn);
+
+  filterBtn.addEventListener("click", () => {
+    const text =
+      document.querySelector("[data-text]").textContent != ""
+        ? document.querySelector("[data-text]").textContent
+        : "Empty";
+    fetch("http://127.0.0.1:5000/predict", {
+      method: "POST",
+      body: new URLSearchParams({
+        text: text,
+      }),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
+      .then((response) => response.json())
+      .then((res) => showModal(res.tweet, res.result))
+      .catch((err) => console.log(err));
+  });
+});
+
 chrome.runtime.onMessage.addListener((req, sender, sendRespon) => {
   if (req.type === "popup-modal") {
     fetch("http://127.0.0.1:5000/predict", {
@@ -15,7 +63,7 @@ chrome.runtime.onMessage.addListener((req, sender, sendRespon) => {
   }
 });
 
-const showModal = (tweet, cat) => {
+const showModal = (tweet = "Nothing!!!", cat = "Nothing!!!") => {
   const modal = document.createElement("div");
   const modalBody = document.createElement("div");
   const tweetText = document.createElement("p");
--- server/server.py
+++ server/server.py
@@ -1,6 +1,9 @@
+from SlangWord import SlangWord
 import pickle
 import re
-
+import pandas as pd
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
@@ -9,13 +12,40 @@ CORS(app)
 model = pickle.load(open('./assets/model/model.pkl', 'rb'))
 
 TEXT_CLEANING_RE = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
-clean_and_transform = lambda tweet: re.sub(TEXT_CLEANING_RE, ' ', tweet.lower()).strip()
+slap_word = SlangWord()
+stopwordFactory = StopWordRemoverFactory()
+stopwords = stopwordFactory.get_stop_words()
+
+wanted_words = ["aku", "anda", "kamu", "kami", "engkau", "beliau", "kau", "dia", "ia", "mereka", "kita", "kalian", "orang", 
+                "ini", "itu", "begini", "begitu", "anak", "bapak", "ibu", "suami", "istri", "ingin", "tidak", 'bukan', 'bukankah', 'bukanlah', 'bukannya',
+                "apa", "dimana", "apakah", "siapa", "mengapa", "kapan", "berapa", "kenapa", "bagaimana", "ya" 
+                ]
+
+
+
+stopwords = [word for word in stopwords if word not in wanted_words]
+stopwordFactory = StopWordRemoverFactory().create_stop_word_remover(stopwords)
+stemmingFactory = StemmerFactory().create_stemmer()
+
+def slap_remove(word):
+    for idx in range(len(slap_word.informalWord)):
+        if slap_word.informalWord[idx] == word:
+            word = slap_word.formalWord[idx]
+    return word
+
+def cleaning(text):
+    text = re.sub(TEXT_CLEANING_RE, " ", text.lower()).strip()
+    text = [slap_remove(word) for word in text.split(" ")]
+    text = " ".join(text)
+    text = stopwordFactory.remove(text)
+    text = stemmingFactory.stem(text)
+    return text
 
 @app.route("/predict", methods=['POST'])
 def predict():
-    text = clean_and_transform(request.form.get('text'))
+    text = cleaning(request.form.get('text'))
     pred = model.predict([text])
-    return jsonify({"tweet": text, "result": pred[0]})
+    return jsonify({"tweet": request.form.get('text'), "result": pred[0]})
 
 if __name__ == "__main__":
     app.run()
