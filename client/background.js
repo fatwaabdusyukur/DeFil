eb9b2fabaad3a62a82886a72c1f6bd38ca70dc92
fatwaabdusyukur <waengyoo@gmail.com>
2023-06-20 17:04:32 +0700

    fixing feature for classify other tweet

--- client/background.js
+++ client/background.js
@@ -10,6 +10,7 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId == "defilMenu") {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {
+        action: "getSelectedElement",
         type: "popup-modal",
         value: info.selectionText,
       });
--- client/content.js
+++ client/content.js
@@ -47,7 +47,10 @@ waitForElm(".css-1dbjc4n.r-1awozwy.r-18u37iz.r-1s2bzr4").then((el) => {
 });
 
 chrome.runtime.onMessage.addListener((req, sender, sendRespon) => {
-  if (req.type === "popup-modal") {
+  if (req.type === "popup-modal" && req.action === "getSelectedElement") {
+    const account = getAccountName(
+      ".css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0"
+    );
     fetch("http://127.0.0.1:5000/predict", {
       method: "POST",
       body: new URLSearchParams({
@@ -58,25 +61,34 @@ chrome.runtime.onMessage.addListener((req, sender, sendRespon) => {
       },
     })
       .then((response) => response.json())
-      .then((res) => showModal(res.tweet, res.result))
+      .then((res) => showModal(res.tweet, res.result, account, true))
       .catch((err) => console.log(err));
   }
 });
 
-const showModal = (tweet = "Nothing!!!", cat = "Nothing!!!") => {
+const showModal = (
+  tweet = "Nothing!!!",
+  cat = "Nothing!!!",
+  account = "Nobody!!!",
+  isExist = false
+) => {
   const modal = document.createElement("div");
   const modalBody = document.createElement("div");
   const tweetText = document.createElement("p");
   const btnClose = document.createElement("button");
   const classTweet = document.createElement("h3");
+  const accountName = document.createElement("h2");
 
   modal.className = "modal";
   modalBody.className = "modal-body";
+  accountName.innerText = `Dari: ${account}`;
+  accountName.className = "account-name";
   tweetText.innerText = tweet;
   tweetText.className = "tweet-text";
   classTweet.innerText = cat;
   classTweet.className = "class-tweet";
   modal.appendChild(modalBody);
+  if (isExist) modalBody.appendChild(accountName);
   modalBody.appendChild(tweetText);
   modalBody.appendChild(classTweet);
   btnClose.innerText = "Close";
@@ -88,4 +100,15 @@ const showModal = (tweet = "Nothing!!!", cat = "Nothing!!!") => {
   btnClose.addEventListener("click", () => {
     document.body.removeChild(modal);
   });
+};
+
+const getAccountName = (el) => {
+  var selectedElement = window.getSelection().anchorNode.parentElement;
+  var topLevelParent = selectedElement.parentNode;
+  for (var i = 0; i < 5; i++) {
+    topLevelParent = topLevelParent.parentNode;
+  }
+
+  const cardHeader = topLevelParent.querySelectorAll(el);
+  return `${cardHeader[0].innerText}(${cardHeader[3].innerText})`;
 };
